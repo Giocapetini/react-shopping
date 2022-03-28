0735198f3ad11dea147a2abc7069326c0f2e4950
Giovanni Capetini <gcapetini@number8.com>
2022-03-28 19:52:09 -0300

    changes;

--- src/App.js
+++ src/App.js
@@ -1,22 +1,113 @@
+import React from 'react'
 import './App.scss';
 import ListItems from './components/list-items/list-items';
 import Receipt from './components/receipt/receipt';
 
-function App() {
-  const items = require('./mocks/items.json');
-  return (
+class App extends React.Component {
 
-    <div className="sales-container">
-      <div className="list">
-        <h1>INPUT</h1>
-        <ListItems items={items} />
+  state = {
+    items: [],
+    newName: '',
+    newValue: '',
+    newTax: '',
+  }
+
+  componentDidMount() {
+    const itemsMock = require('./mocks/items.json');
+    this.setState({ items: itemsMock });
+  }
+
+  addNewItem(itemName, itemValue, itemTax) {
+    const items = this.state.items;
+
+    if (itemName !== '' && itemValue !== 0 && itemTax !== 0)
+      items.push({
+
+        "id": items.length,
+        "itemName": itemName,
+        "itemValue": Number.parseInt(itemValue, 10),
+        "selectedAmount": 0,
+        "sumUntaxed": 0,
+        "sumtaxed": 0,
+        "tax": Number.parseInt(itemTax, 10)
+
+      })
+
+    this.setState({
+      items: items,
+      newName: '',
+      newValue: '',
+      newTax: '',
+    })
+  }
+
+  handleNewName(event) {
+    let modName = event.target.value;
+
+    this.setState({ newName: modName });
+  }
+
+  handleNewValue(event) {
+    let modValue = event.target.value;
+
+    this.setState({ newValue: modValue });
+  }
+  handleNewTax(event) {
+    let modValue = event.target.value;
+
+    this.setState({ newTax: modValue });
+  }
+
+  handleSelectedAmount(e) {
+    console.log('e', e)
+  }
+  render() {
+
+    const list = this.state.items.map((item, i) => {
+      return (<div className='list-content' key={item.id}>
+        <ListItems item={item} />
+        <input type="number" min={0} max={99}
+          onChange={this.handleSelectedAmount}></input>
+      </div>)
+    })
+
+    const newItem = <div className='new-item-container'>
+      <input id='newItemName' placeholder='New Item Name' type="text" value={this.state.newName} onChange={this.handleNewName.bind(this)} required ></input>
+      <div className='values-container'>
+        <input id='newItemValue' placeholder='New Item Value' type="number" value={this.state.newValue} onChange={this.handleNewValue.bind(this)} step='any' min={0}
+          required></input>
+        <input id='newItemtax' placeholder='New Item Tax %' type="number" value={this.state.newTax} onChange={this.handleNewTax.bind(this)} step='any' min={0}
+          required></input>
       </div>
-      <div className="receipt">
-        <h1>OUTPUT</h1>
-        <Receipt />
+      <button id='addNewItem' onClick={(e) => {
+        this.addNewItem(this.state.newName, this.state.newValue, this.state.newTax)
+      }}>+</button>
+    </div >
+
+
+    return (
+      <div className="sales-container" >
+        <div className="list">
+          <h1>INPUT</h1>
+          <div className='header'>
+            <h2>Name</h2>
+            <h2>Value</h2>
+            <h2>Tax</h2>
+            <h2>Amount</h2>
+          </div>
+          <>
+            {list}
+          </>
+          <>
+            {newItem}
+          </>
+        </div>
+        <div className="receipt">
+          <h1>OUTPUT</h1>
+          <Receipt />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
--- src/components/list-items/list-items.js
+++ src/components/list-items/list-items.js
@@ -1,35 +1,15 @@
 import React from 'react'
 import './list-item.scss';
-class ListItems extends React.Component {
+const ListItems = (props) => {
+    return (
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: 0,
-        }
-    }
+        <div className="list-item">
+            <h2>{props.item.itemName}</h2>
+            <p>$ {props.item.itemValue}</p>
+            <p>% {props.item.tax}</p>
+        </div>
 
-    handleChange(i) {
-        this.setState({ value: event.target.value })
-    }
-
-    render() {
-        return (
-            <div className="list-container">
-                {this.props.items.map((item) => {
-                    return <div key={item.id} className="list-item">
-                        <h2>{item.itemName}</h2>
-                        <p>$ {item.itemValue}</p>
-                        <input type="number" min={0} max={99}
-                            value={this.state.value}
-                            onChange={this.handleChange}></input>
-                        {this.state.value}
-                    </div>
-                })}
-
-            </div >
-        );
-    }
+    );
 }
 
 export default ListItems;
